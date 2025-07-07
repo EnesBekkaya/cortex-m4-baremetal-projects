28fecfb8185193d78442bd679989eac6f8658b39
Enes Bekkaya <enes_bekkaya_@hotmail.com>
2025-07-08 02:59:18 +0300

    Configure timer for PWM signal generation

---
+++ timer-pwm/src/firmware.c
@@ -0,0 +1,39 @@
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
+#include <core/system.h>
+#include <../inc/timer.h>
+#define LED_PORT 	 (GPIOG)
+#define LED_PIN  	 (GPIO13)
+
+
+
+static void gpio_setup(void){
+	rcc_periph_clock_enable(RCC_GPIOG);
+	gpio_mode_setup(LED_PORT,GPIO_MODE_AF,GPIO_PUPD_NONE,LED_PIN);
+	gpio_set_af(LED_PORT,GPIO_AF1,LED_PIN);
+}
+
+
+int main(void)
+{
+	system_setup();
+	gpio_setup();
+	timer_setup();
+
+	uint64_t startTime=system_get_ticks();
+	float duty_cycle=0.0f;
+	timer_pwm_set_duty_cycle(duty_cycle);
+	while(1){
+		if(system_get_ticks()-startTime >=10){
+			duty_cycle+=1.0f;
+			if(duty_cycle>100.0f){
+				duty_cycle=0.0f;
+			}
+			timer_pwm_set_duty_cycle(duty_cycle);
+
+			startTime=system_get_ticks();
+		}
+	
+	}
+	return 0;
+}
