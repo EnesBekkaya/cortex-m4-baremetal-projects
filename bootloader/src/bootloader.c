f12d6321bfdb650a5e0d6e26b09bb1b357575e99
Enes Bekkaya <enes_bekkaya_@hotmail.com>
2025-07-15 00:26:44 +0300

    Add basic UART driver

--- bootloader/src/bootloader.c
+++ bootloader/src/bootloader.c
@@ -1,6 +1,7 @@
 #include "common-defines.h"
 #include "libopencm3/stm32/memorymap.h"
 #include <libopencm3/cm3/scb.h>
+#include <libopencm3/cm3/vector.h>
 
 #define BOOTLOADER_SIZE (0x8000U)
 #define MAIN_APP_START_ADRESS (FLASH_BASE + BOOTLOADER_SIZE)
@@ -11,21 +12,16 @@ static inline void set_msp(uint32_t top_of_main_stack)
 
 static void jump_to_main(void)
 {
-	typedef void (*void_fn)(void);
-	
 	uint32_t msp_value = *(volatile uint32_t *)MAIN_APP_START_ADRESS;
 
-	uint32_t *reset_vector_entry = (uint32_t *)(MAIN_APP_START_ADRESS + 4U);
-	uint32_t *reset_vector = (uint32_t *)(*reset_vector_entry);
+	vector_table_t *main_vector_table = (vector_table_t *)(MAIN_APP_START_ADRESS);
 
-	void_fn jump_fn = (void_fn)reset_vector;
 
 	SCB_VTOR = MAIN_APP_START_ADRESS;
 	set_msp(msp_value);
+	main_vector_table->reset();
 
-	jump_fn();
 }
-
 int main(void)
 {
 
--- timer-pwm/src/firmware.c
+++ timer-pwm/src/firmware.c
@@ -1,16 +1,26 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 #include <core/system.h>
+#include <core/uart.h>
+#include <string.h>
+
 #include <../inc/timer.h>
 #define LED_PORT 	 (GPIOG)
 #define LED_PIN  	 (GPIO13)
 
-
+#define UART_PORT    (GPIOA)
+#define RX_PIN     	 (GPIO10)
+#define TX_PIN     	 (GPIO9)
 
 static void gpio_setup(void){
 	rcc_periph_clock_enable(RCC_GPIOG);
+	rcc_periph_clock_enable(RCC_GPIOA);
+
 	gpio_mode_setup(LED_PORT,GPIO_MODE_AF,GPIO_PUPD_NONE,LED_PIN);
 	gpio_set_af(LED_PORT,GPIO_AF1,LED_PIN);
+
+	gpio_mode_setup(UART_PORT,GPIO_MODE_AF,GPIO_PUPD_NONE,RX_PIN | TX_PIN);
+	gpio_set_af(UART_PORT,GPIO_AF7,RX_PIN | TX_PIN);
 }
 
 
@@ -19,6 +29,7 @@ int main(void)
 	system_setup();
 	gpio_setup();
 	timer_setup();
+	uart_setup();
 
 	uint64_t startTime=system_get_ticks();
 	float duty_cycle=0.0f;
@@ -33,6 +44,11 @@ int main(void)
 
 			startTime=system_get_ticks();
 		}
+
+		 if(uart_data_available()){
+			uint8_t data= uart_read_byte();
+			uart_write_byte(data + 1 );
+		 }
 	
 	}
 	return 0;
