5977926fbbe81be8ecb39eed57cb3e65e576fffc
Enes Bekkaya <enes_bekkaya_@hotmail.com>
2025-08-03 21:04:13 +0300

    Complete packet communications module

--- bootloader/inc/comms.h
+++ bootloader/inc/comms.h
@@ -8,6 +8,9 @@
 #define PACKET_CRC_BYTES    (1)
 #define PACKET_LENGTH       (PACKET_LENGTH_BYTES + PACKET_DATA_LENGTH + PACKET_CRC_BYTES)
 
+#define PACKET_RETX_DATA0 (0x19)
+#define PACKET_ACK_DATA0  (0x15)
+
 typedef struct comms_packet_t
 {
     uint8_t length;
@@ -21,5 +24,5 @@ void comms_update(void);
 bool comms_packet_available(void);
 void comms_write(comms_packet_t* packet);
 void comms_read(comms_packet_t* packet);
-
+uint8_t comms_compute_crc(comms_packet_t* packet);
 #endif 
